c750d160bef6a54160b622eff10208ed70315313
TheAndresG <andresgomezguardamagna@gmail.com>
2023-01-09 11:53:37 -0300

    juego completo

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
+import { calculateWinner } from "../helpers";
 import Board from "./Board";
 
-const Game = () => {
-  const handleClick = () => {};
-  const jumpTo = () => {};
-  const renderMoves = () => {};
+const style = {
+  width: "208px",
+  margin: "28px auto",
+};
 
-  return <Board onClick={handleClick} />;
+const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [isNext, setIsNext] = useState(true);
+
+  const winner = calculateWinner(board);
+  console.log(winner);
+
+  const handleClick = (i) => {
+    const copyBoard = [...board];
+    if (winner || copyBoard[i]) return;
+    copyBoard[i] = isNext ? "X" : "O";
+    setBoard(copyBoard);
+    setIsNext(!isNext);
+  };
+  const renderMoves = () => {
+    return (
+      <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+    );
+  };
+
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />;
+      <div style={style}>
+        <p>
+          {winner ? `Winner ${winner} ` : `Next player ${isNext ? "X" : "O"}`}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
 };
 
 export default Game;
